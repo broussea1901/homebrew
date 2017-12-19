1ca5b8865ff9ff357c9b44c50cb9fe8fb27dbbbd
Broussea1901 <26170335+broussea1901@users.noreply.github.com>
2017-12-19 10:49:41 +0100

    Create hive@1.2.rb

---
+++ hive@1.2.rb
@@ -0,0 +1,35 @@
+class Hive < Formula
+  desc "Hadoop-based data summarization, query, and analysis"
+  homepage "https://hive.apache.org"
+  url "https://www.apache.org/dyn/closer.cgi?path=hive/hive-1.2.2/apache-hive-1.2.2-bin.tar.gz"
+  sha256 "763b246a1a1ceeb815493d1e5e1d71836b0c5b9be1c4cd9c8d685565113771d1"
+
+  bottle :unneeded
+
+  depends_on :java => "1.7+"
+  depends_on "hadoop"
+
+  def install
+    rm_f Dir["bin/*.cmd", "bin/ext/*.cmd", "bin/ext/util/*.cmd"]
+    libexec.install %w[bin conf examples hcatalog lib scripts]
+
+    Pathname.glob("#{libexec}/bin/*") do |file|
+      next if file.directory?
+      (bin/file.basename).write_env_script file,
+        Language::Java.java_home_env("1.7+").merge(:HIVE_HOME => libexec)
+    end
+  end
+
+  def caveats; <<~EOS
+    Hadoop must be in your path for hive executable to work.
+
+    If you want to use HCatalog with Pig, set $HCAT_HOME in your profile:
+      export HCAT_HOME=#{opt_libexec}/hcatalog
+    EOS
+  end
+
+  test do
+    system bin/"schematool", "-initSchema", "-dbType", "derby"
+    assert_match "Hive #{version}", shell_output("#{bin}/hive --version")
+  end
+end
